1fbd841038d5670181976eefd2ffa529971651b3
Muieen Ahmed <ahmedmuieen@gmail.com>
2022-05-18 09:01:07 +0530

    Create ceasercipher.py

---
+++ ceasercipher.py
@@ -0,0 +1,12 @@
+print("Enter message to encrypt")
+msg = input()
+print("Enter the key value")
+key = int(input())
+encrptedMsg = ""
+for i in range(len(msg)):
+	char = msg[i]
+	encrptedMsg += chr((ord(char) + key - 97) % 26 + 97)
+  
+print ("Orginal Message : " + msg)
+print ("Key: " + str(key))
+print ("Encrpyted Message: " + encrptedMsg)
